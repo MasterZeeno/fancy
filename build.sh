0ab0d186e68aa6ee8c0376b51c9063c1e5a3f9f3
MasterZeeno <masterzeeno@outlook.com>
2025-06-17 12:10:39 +0800

    Update

---
+++ build.sh
@@ -0,0 +1,150 @@
+TERMUX_PKG_DESCRIPTION="A command-line tool to generate, analyze, convert and manipulate colors"
+TERMUX_PKG_MAINTAINER="MasterZeeno <masterzeeno@outlook.com>"
+TERMUX_PKG_HOMEPAGE=https://github.com/MasterZeeno/fancy
+TERMUX_PKG_LICENSE_FILE="LICENSE-MIT, LICENSE-APACHE"
+TERMUX_PKG_LICENSE=MIT
+TERMUX_PKG_VERSION=0.10.0
+TERMUX_PKG_SRCURL=https://github.com/sharkdp/pastel/archive/refs/heads/master.zip
+TERMUX_PKG_SHA256=823b3c44a10235372fa55b5fa1d5c5565bc8ffb19a8d85846a45b2142ae1765a
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BREAKS=pastel
+TERMUX_PKG_REPLACES=pastel
+TERMUX_PKG_DEPENDS=zenity
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_post_get_source() {
+  local DIST_REPO_OWNER="${TERMUX_PKG_MAINTAINER%% *}"
+  local DIST_REPO_NAME="${TERMUX_PKG_NAME}"
+  local SRC_REPO_OWNER="${TERMUX_PKG_SRCURL:19:7}"
+  local SRC_REPO_NAME="${TERMUX_PKG_BREAKS}"
+
+  case_fix() {
+    local str="${1:?}"
+    local idx="${2:-${#str}}"
+    
+    case "${str}" in "${DIST_REPO_OWNER}")
+      echo "${str}"; return ;; esac
+      
+    echo "${str}" | sed -E "s/^(.{0,${idx}})/\U\1/"
+  }
+
+  print_msg() {
+    (($#)) || return
+    local old new file head="${1:?}"
+    if [[ $# -gt 1 ]]; then
+      old="${1:?}" new="${2:?}"
+      file="${3:-}" head='success'
+    fi
+
+    local R=$'\e[0m'
+    local I=$'\uf00c' D=$'\uea83' S=$'\ueb8d' F=$'\uf4a5'
+    local DG=$'\e[2;32m' BG=$'\e[1;32m' BY=$'\e[1;33m'
+    local DIRXPR=$(printf "s|%s|${BY}\$%s${BG}|;" \
+      "${HOME}" 'HOME' "${PREFIX}" 'PREFIX')
+
+    printf " ${BG}%s${R}\n" "${I} ${head^}"
+    [[ $# -eq 1 ]] && { sleep 1; clear; return; }
+    
+    local icon="${S}" key='string'
+    local -a paths=('file')
+    
+    if [[ -z ${file:-} ]]; then
+      [[ -d ${old} ]] && \
+        icon="${D}" || icon="${F}"
+      key='path' paths=('old' 'new')
+    fi
+
+    for v in "${paths[@]}"; do
+      local -n var="${v}"
+      var="$(realpath -mPq "${var}" \
+        | sed "${DIRXPR}")"
+    done
+
+    for v in {old,new,file}; do
+      local -n var="${v}"; [[ -z ${var:-} ]] && continue
+      case "${v}" in file) icon="${F}"; key='path' ;; esac
+      printf " ${DG}%s:${R} ${BG}%s${R}\n" \
+        "${icon} ${v^} ${key^}" "${var}"
+    done
+    
+    echo; sleep 0.32
+  }
+
+  clear
+
+  for prop in REPO_{OWNER,NAME}; do
+    for p in {SRC,DIST}; do
+      local -n "${p}=${p}_${prop}"
+    done
+
+    print_msg "Renaming and replacing [${SRC}] to [${DIST}]..."
+
+    find -P "${TERMUX_PKG_SRCDIR}" -mindepth 1 -readable \
+      -writable ! -name '.*' | while IFS= read -r ITEM; do
+      for i in {0,1,${#SRC}}; do
+        local SRC_CASED=$(case_fix "${SRC}" $i)
+        local DIST_CASED=$(case_fix "${DIST}" $i)
+
+        if [[ ${ITEM} == *"${SRC_CASED}"* ]]; then
+          local NEW_NAME="${ITEM//${SRC_CASED}/${DIST_CASED}}"
+          mv -f "${ITEM}" "${NEW_NAME}"; ITEM="${NEW_NAME}"
+          print_msg "${ITEM}" "${NEW_NAME}"
+        fi
+
+        if [[ -f ${ITEM} ]]; then
+          local MIMETYPE=$(file --mime-type -b "${ITEM}")
+          if [[ ${MIMETYPE} == text/* ]]; then
+            if [[ ${ITEM##*.} == toml ]]; then 
+              local A='authors =' AVAL=$(grep -om1 "^${A}.*" "${ITEM}")
+              if [[ -n ${AVAL} ]]; then sed -Ei "$(printf \
+                "s/^${A}.*/${A} [\"%s\"]/" "${TERMUX_PKG_MAINTAINER}")" "${ITEM}"
+                print_msg "$(echo "${AVAL}" | sed -E 's/.*=.*\["(.*)"\]/\1/')" \
+                  "${TERMUX_PKG_MAINTAINER}" "${ITEM}"
+              fi
+            fi
+            if grep -q "${SRC_CASED}" "${ITEM}"; then
+              sed -i "s/${SRC_CASED}/${DIST_CASED}/g" "${ITEM}"
+              print_msg "${SRC_CASED}" "${DIST_CASED}" "${ITEM}"
+            fi
+          fi
+        fi
+      done
+    done
+  done
+
+  print_msg 'Starting build...'
+}
+
+termux_step_pre_configure() {
+  termux_setup_rust
+  : "${CARGO_HOME:=${HOME}/.cargo}"
+  export CARGO_HOME
+  cargo fmt -- --check
+  cargo clippy --locked --all-targets
+  cargo test --locked
+  cargo fetch --locked --target "${CARGO_TARGET_NAME}"
+}
+
+termux_step_make() {
+  SHELL_COMPLETIONS_DIR="${TERMUX_PKG_BUILDDIR}/completions" \
+    cargo build --jobs "${TERMUX_PKG_MAKE_PROCESSES}" \
+      --locked --target "${CARGO_TARGET_NAME}" --release
+}
+
+termux_step_make_install() {
+  local -A _SHELLS=(
+    [zsh]='/site-functions'
+    [bash]='-completion/completions'
+    [fish]='/vendor_completions.d' )
+
+  install -Dm755 -t "${TERMUX_PREFIX}/bin" \
+    "target/${CARGO_TARGET_NAME}/release/${TERMUX_PKG_NAME}"
+
+  for _SHELL in "${!_SHELLS[@]}"; do local _FILE
+    case "${_SHELL}" in zsh) _FILE="_${TERMUX_PKG_NAME}" ;;
+      *) _FILE="${TERMUX_PKG_NAME}.${_SHELL}" ;; esac
+      
+    install -Dm600 "${TERMUX_PKG_BUILDDIR}/completions/${_FILE}" \
+      "${TERMUX_PREFIX}/share/${_SHELL}${_SHELLS[$_SHELL]}/${_FILE}"
+  done
+}
---
+++ updater.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+: "${LC_ALL:="C"}"
+: "${BUILD_SH:="$(pwd)/build.sh"}"
+: "${FUNC_SH:="$(pwd)/functions.sh"}"
+: "${OFF_TOML:="$(pwd)/offline.toml"}"
+export LC_ALL BUILD_SH FUNC_SH OFF_TOML
+
+get_ver() { echo "$1" | grep -im1 version | cut -d= -f2 | sed 's/[^0-9.]//g'; }
+print_msg() { clear; echo -ne '\e[1;32m'; echo -e "$1"; echo -ne '\e[0m'; sleep 0.69; }
+
+update() {
+  print_msg "Checking ${DIST_REPO_NAME} build script for updates..."
+  
+  if ((FORCE)); then
+    :> "${BUILD_SH}"
+    SRC_TOML=$(curl -sL "${SRC_TOML_URL}" \
+      | awk 'NR>1&&NF{print}/^ *$/{exit}' \
+      | sed 's/ *= */=/g;s/[]["]//g' \
+      | tee "${OFF_TOML}")
+    SRC_ZIP_SHA=$(curl -sL "${SRC_ZIP_URL}" \
+      | sha256sum | awk '{print $1}')
+  fi
+  
+  local LVER=$(get_ver "$(cat "${BUILD_SH}")") RVER=$(get_ver "$(cat "${OFF_TOML}")")
+  local LATEST=$(printf '%s\n' "${LVER:-0}" "${RVER:-69}" | sort -V | tail -n1)
+  [[ "${LATEST}" == "${LVER}" ]] && print_msg "latest: (v${LATEST})" && exit
+  print_msg "Updating ${DIST_REPO_NAME} build script to (v${LATEST})..."
+  
+  local -a PROPS=(
+    homepage description license version build_in_src=true auto_update=true
+    maintainer="${DIST_AUTHOR}" srcurl="${SRC_ZIP_URL}" sha256="${SRC_ZIP_SHA}"
+    breaks="${SRC_REPO_NAME}" replaces="${SRC_REPO_NAME}" depends='zenity'
+  )
+
+  local HP="${PROPS[0]}" LIC="${PROPS[2]}"
+  local PX='TERMUX_PKG' LIC_FILE="_${LIC}_file"
+  
+  ( echo "${SRC_TOML}" | grep -o "${LIC}=.*" | sed -E \
+    "s/-.*//g;s/=/-/;s/^(.*)\/(.*)/\U${LIC_FILE}=\1, ${LIC}-\2/";
+    printf '_%s\n' "${PROPS[@]:4}"; echo "${SRC_TOML}" ) \
+      | sed -E "s/(.*${LIC}=.*)\/.*/\1/;s|${HP}=.*|${HP}=${DIST_REPO_URL}|" \
+      | sed $(echo -n 's/^\('; printf '%s\\|' "${PROPS[@]:0:4}" \
+      | sed 's/|$/\)/'; echo -n "=\(.*\)/_\1=\2/;/^_/!d;s/\(^.*\)=/\U${PX}\1=/") \
+      | sort -u | awk -F= '{if($2~/ /){print $1"=\""$2"\""}else{print $1"="$2}}' \
+        > "${BUILD_SH}.tmp"
+
+  local -A SORT_MAP=(
+    [Z1]='description' [Z2]='maintainer' [Z3]='homepage'
+    [Z4]='license_file' [Z5]='license' [Z6]='version'
+    [Z7]='srcurl' [Z8]='sha256' [Z9]='auto_update'
+    [Z10]='breaks' [Z11]='replaces' [Z12]='depends'
+    [Z13]='build_in_src' )
+  
+  for k in Z{1..13}; do
+    grep -Eiom1 "^${PX}_${SORT_MAP[$k]^^}=.*" \
+    "${BUILD_SH}.tmp" >> "${BUILD_SH}"
+  done
+  
+  cat "${FUNC_SH}" >> "${BUILD_SH}"; rm -rf "${BUILD_SH}.tmp"
+  print_msg "Update success! ${DIST_REPO_NAME} build script (v${LATEST})"
+}
+
+DIST_REPO_GIT=$(git config --local remote.origin.url || \
+  echo https://github.com/MasterZeeno/fancy.git)
+
+DIST_REPO_URL=$(echo "${DIST_REPO_GIT}" | sed -E 's/.git$//')
+PCL=$(echo "${DIST_REPO_GIT}" | cut -c1-8)
+GH_DOMAIN=$(echo "${DIST_REPO_URL}" | cut -d'/' -f3)
+GH=$(echo "${GH_DOMAIN}" | sed -E 's/.com$//')
+GH_RAW="${PCL}raw.${GH}usercontent.com"
+GH_API="${PCL}api.${GH_DOMAIN}/repos"
+GH_HOME="${PCL}${GH_DOMAIN}"
+
+DIST_REPO_OWNER=$(echo "${DIST_REPO_URL}" | cut -d'/' -f4)
+DIST_REPO_NAME=$(echo "${DIST_REPO_URL}" | cut -d'/' -f5)
+DIST_REPO_EMAIL="${DIST_REPO_OWNER,,}@outlook.com"
+DIST_AUTHOR="${DIST_REPO_OWNER} <${DIST_REPO_EMAIL}>"
+
+SRC_REPO_OWNER='sharkdp' SRC_REPO_NAME='pastel'
+SRC_REPO_URL="${GH_HOME}/${SRC_REPO_OWNER}/${SRC_REPO_NAME}"
+SRC_RAW_URL="${GH_RAW}/${SRC_REPO_OWNER}/${SRC_REPO_NAME}"
+SRC_ZIP_URL="${SRC_REPO_URL}/archive/refs/heads/master.zip"
+SRC_TOML_URL="${SRC_RAW_URL}/refs/heads/master/Cargo.toml"
+
+FORCE=0;
+case "$1" in -f) FORCE=1 ;; esac
+[[ ! -f "${BUILD_SH}" ]] && FORCE=1
+((FORCE)) && :> "${BUILD_SH}"
+
+update
