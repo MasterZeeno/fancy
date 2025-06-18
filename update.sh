2c4f8bc631983a8156051f880e96b7de02342104
MasterZeeno <MasterZeeno masterzeeno@outlook.com>
2025-06-18 16:23:47 +0800

    Bumped:

--- update.sh
+++ update.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-get_ver() { (cat "$1" 2>/dev/null || echo "$1") | grep -iom1 'version[ =].*' | sed 's|[^0-9.]||g'; }
+get_ver() {
+  ([[ -f "$1" ]] && \
+    cat "$1" || echo "$1") \
+    | grep -iom1 'version[ =].*' \
+    | sed 's|[^0-9.]||g'
+}
 
 print_msg() {
   clear
@@ -17,14 +22,13 @@ FUNC_SH="$(pwd)/func.sh" BUILD_SH="$(pwd)/build.sh"
 touch "$BUILD_SH" "$FUNC_SH"
 
 DIST_OWNER="MasterZeeno" DIST_REPO="fancy"
+DIST_EMAIL="$DIST_OWNER <${DIST_OWNER,,}@outlook.com>"
 SRC_OWNER="sharkdp" SRC_REPO="pastel"
-
-BASE_MSG="$DIST_OWNER/$DIST_REPO"
 
 print_msg "checking updates for"
 
 SRC_TOML=$(curl -fsSL "https://raw.githubusercontent.com/$SRC_OWNER/$SRC_REPO/refs/heads/master/Cargo.toml")
-CURRENT_VER=$(get_ver "$BUILD_SH") LATEST_VER=$(get_ver "$SRC_TOML") BASE_MSG+=" to v$LATEST_VER"
+CURRENT_VER=$(get_ver "$BUILD_SH") LATEST_VER=$(get_ver "$SRC_TOML")
 
 [[ "$1" == '-f' ]] && CURRENT_VER=0
 if ! printf '%s\n' "$CURRENT_VER" "$LATEST_VER" \
@@ -34,7 +38,6 @@ if ! printf '%s\n' "$CURRENT_VER" "$LATEST_VER" \
   
   SRC_ZIP_URL="https://github.com/$SRC_OWNER/$SRC_REPO/archive/refs/heads/master.zip"
   SRC_ZIP_SHA=$(curl -fsSL "${SRC_ZIP_URL}" | sha256sum | awk '{print $1}')
-  
   { echo "$SRC_TOML" \
       | awk 'NR==1{next}/^ *$/{exit}NF{gsub(/\[|\]|"/,"")gsub(/ += +/,"=");print}' \
       | sed -E \
@@ -42,8 +45,7 @@ if ! printf '%s\n' "$CURRENT_VER" "$LATEST_VER" \
         -e "s|^(homepage)=.*|_\1=https://github.com/$DIST_OWNER/$DIST_REPO|" \
         -e "/^license/{s|[0-9.-]||g; s|.*|_\U&|; p; s|_(.*)=(.*)/(.*)|_\1_file=\1-\2, \1-\3|}"
     printf '_%s=%s\n' \
-      srcurl "$SRC_ZIP_URL" sha256 "$SRC_ZIP_SHA" \
-      maintainer "$DIST_OWNER <${DIST_OWNER,,}@outlook.com>" \
+      srcurl "$SRC_ZIP_URL" sha256 "$SRC_ZIP_SHA" maintainer "$DIST_EMAIL" \
       breaks "$SRC_REPO" replaces "$SRC_REPO" build_in_src true auto_update true
   } | sed -E "/^_/!d; s|(.*)=|\Utermux_pkg\1=|;s|=(.*)|=\"\1\"|g" \
     | awk '{print length, $0}' | sort -nr | cut -d' ' -f2- > "$BUILD_SH"
@@ -52,7 +54,7 @@ if ! printf '%s\n' "$CURRENT_VER" "$LATEST_VER" \
 fi
 
 ( git config user.name "$DIST_OWNER"
-  git config user.email "${DIST_OWNER,,}@outlook.com"
+  git config user.email "$DIST_EMAIL"
   git pull; git add .
   git commit -m "Bumped: $BASE_MSG"
   git push ) &>/dev/null
