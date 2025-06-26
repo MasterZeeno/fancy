fc499265bbef2955f0df9af21e6be19d1d4b7bec
MasterZeeno <masterzeeno@outlook.com>
2025-06-26 23:17:45 +0800

    Update

--- update.sh
+++ update.sh
@@ -6,23 +6,23 @@ if [ -z "${BASH_VERSION:-}" ]; then
 fi
 
 MAIN_DIR=$(git rev-parse --show-toplevel 2>/dev/null || pwd)
-OUT_DIR="$MAIN_DIR/output"; rm -rf "$OUT_DIR"
+OUT_DIR="$MAIN_DIR/output" CFG="$MAIN_DIR/.config"
 
-for v in {dist,src,build}.{owner,repo,email} {funcs,build}.sh; do
+for v in {user,src,build}.{name,repo,email} {funcs,build}.sh; do
   val="" condition=false
   if [[ $v =~ \.sh$ ]]; then
     val="$MAIN_DIR/$v"
     [[ -s "$val" ]] && condition=true
   else
-    val=$(git config --file "$MAIN_DIR/.cfg" $v)
+    val=$(git config --file "$CFG" $v)
     [[ -n "$val" ]] && condition=true
   fi
   
   if [[ "$condition" == "true" ]]; then
-    declare -u var="${v/./_}"
+    var="${v/./_}"
     declare "${var^^}=$val"
   else
-    if [[ $v =~ \.sh$ || $v =~ ^[dsb]*(owner|repo)$ || $v =~ ^d*email$ ]]; then
+    if [[ $v =~ \.sh$ || $v =~ ^[usb]*(name|repo)$ || $v =~ ^u*email$ ]]; then
       echo "Variable '$var' is necessary, provide a valid value."
       return 1 2>/dev/null || exit 1
     fi
@@ -45,7 +45,7 @@ else
 fi
 
 if [[ -z "${TERMUX_APP_PACKAGE_MANAGER:-}" ]]; then
-  fancy_print --preset=failed +b "Error: no package Manager defined"
+  fancy_print --preset=failed +b "Error: no package Manager defined" >&2
   return 1 2>/dev/null || exit 1
 fi
 
@@ -71,12 +71,14 @@ install_pkgs() {
       esac
       ((retry++)); if ((retry>3)); then status="failed"; break; fi
     done
+
+    if [[ "$status" == "failed" ]]; then
+      FANCY_ARGS=(--no-print --preset="$status")
+      fancy_print -n +d "Install $status:"
+      fancy_print --print --no-icon +b "$pkg" >&2
+      exit 1
+    fi
     
-    FANCY_ARGS=(--no-print --preset="$status")
-    fancy_print -n +d "Install $status:"
-    fancy_print --print --no-icon +b "$pkg"
-    
-    [[ "$status" == "failed" ]] && exit 1
     shift
   done
   
@@ -85,7 +87,7 @@ install_pkgs() {
 
 get_ver() { ([[ -f "$1" ]] && cat "$1" || echo "$1") | grep -iom1 'version[ =].*' | grep -Eo '[0-9.]+'; }
 print_update_msg() {
-  local msg="${1:?}" slp="${2:-0}" bmsg="$DIST_OWNER/$DIST_REPO"
+  local msg="${1:?}" slp="${2:-0}" bmsg="$USER_NAME/$USER_REPO"
   [[ -z "${LATEST_VERSION:-}" ]] || bmsg+=" to v$LATEST_VERSION"
   
   FANCY_ARGS=(--no-print)
@@ -100,93 +102,99 @@ print_update_msg() {
 }
 
 build_fancy() {
-  mkdir -p "$OUT_DIR"
-  
-  [[ -d "$MAIN_DIR/$BUILD_REPO" ]] || \
-    git clone --quiet "https://github.com/$BUILD_OWNER/$BUILD_REPO.git"
+  [[ -d "$MAIN_DIR/$BUILD_REPO" ]] || git clone --quiet "https://github.com/$BUILD_NAME/$BUILD_REPO.git"
   cd "$MAIN_DIR/$BUILD_REPO" && git pull --quiet &>/dev/null || exit 1
   
   export TERM="${TERM:-"xterm-256color"}"
+  cd "$MAIN_DIR/$BUILD_REPO/scripts" || exit 1
   
-  cd "$MAIN_DIR/$BUILD_REPO/scripts"
-  
-  source properties.sh || true
+  source properties.sh &>/dev/null || true
   if [[ "$RUNNER" != "termux" ]]; then
     [[ ! -d "${NDK:-}" || ! -d "${ANDROID_HOME:-}" ]] \
-      && source setup-android-sdk.sh || true
+      && source setup-android-sdk.sh &>/dev/null || true
   fi
+  
+  source setup-$RUNNER.sh &>/dev/null || true
 
   if [[ "$RUNNER" == "archlinux" ]]; then
     $SUDO sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-    local -a packages=(ncurses5-compat-libs makedepend python2)
-    local -a install_opts=(--noconfirm --needed)
-    if command -v paru &>/dev/null; then
-      $SUDO paru -S "${install_opts[@]}" "${packages[@]}" &>/dev/null
-    elif command -v yay &>/dev/null; then
-      $SUDO yay -S "${install_opts[@]}" "${packages[@]}" &>/dev/null
-    else
-      cd "$MAIN_DIR"
-      for package in "${packages[@]}"; do
-        [[ -d "$package" ]] && rm -rf "$package"
-        git clone --quiet https://aur.archlinux.org/"$package"
-        cd "$package" || exit 1
-        makepkg -si --skippgpcheck "${install_opts[@]}" &>/dev/null
-        cd "$MAIN_DIR" && rm -rf "$package"
+    local -a pkgs=(ncurses5-compat-libs makedepend python2)
+    local -a opts=(--noconfirm --needed)
+    local install_cmd
+    
+    for cmd in paru yay; do
+      if command -v "$cmd" &>/dev/null; then 
+        install_cmd="$cmd"
+        break
+      fi
+    done
+    
+    for pkg in "${pkgs[@]}"; do
+      local status="success" retry=0 pkgdir="$TMPDIR/$pkg"
+      while ! pacman -Q "$pkg" &>/dev/null; do
+        if [[ -n "${install_cmd:-}" ]]; then
+          $SUDO $install_cmd -S "${opts[@]}" "$pkg" &>/dev/null
+        else
+          git clone --quiet -o "$pkgdir" "https://aur.archlinux.org/$pkg" &>/dev/null
+          cd "$pkgdir" &>/dev/null && makepkg -si --skippgpcheck "${opts[@]}" &>/dev/null
+        fi
+        [[ $? -ne 0 ]] && status="failed"; ((retry++)); ((retry>3)) && break
       done
-    fi
+      rm -rf "$pkgdir"
+      if [[ "$status" == "failed" ]]; then
+        echo
+        FANCY_ARGS=(--no-print --preset="$status")
+        fancy_print -n +d "Install $status:"
+        fancy_print --print +b "$pkg" >&2
+        exit 1
+      fi
+    done
   fi
 
-  source setup-$RUNNER.sh || true
-
   cd "$MAIN_DIR/$BUILD_REPO" && ./clean.sh &>/dev/null
-  
-  ./build-package.sh -f -q -o "$OUT_DIR/" "$MAIN_DIR"
-  
+  rm -rf "$OUT_DIR"; mkdir -p "$OUT_DIR"
+  ./build-package.sh -q -f -o "$OUT_DIR/" "$MAIN_DIR"
   find "$OUT_DIR" -iname '*.deb' | grep -q . && publish_fancy
 }
 
 gh_login() {
-  if [[ "$(gh api user --jq .login)" != "$DIST_OWNER" ]]; then
+  if [[ "$(gh api user --jq .login)" != "$USER_NAME" ]]; then
     gh auth logout &>/dev/null
     
     local status="success"
     cd "$MAIN_DIR"
     
     for var in name email; do
-      local val=$(git config --file "$MAIN_DIR/.cfg" user.$var)
+      local val="USER_${var^^}"
       for flag in global local; do
-        [[ "$(git config --$flag user.$var)" != "$val" ]] \
-          && git config --$flag user.$var "$val"
+        git config --$flag user.$var "${!val}"
       done
     done
     
     echo
     FANCY_ARGS=(--no-print --color=36)
-    fancy_print +d -n "Logging into"
+    fancy_print +d -n "Logging into:"
     fancy_print +b --print "Github"
     
     if [[ -z "${GH_TOKEN:-}" ]]; then
-      local token
       while true; do
-        fancy_print -n "*GH Auth Token: "
-        read -r token
-        token="${token//[^a-zA-Z0-9]/}"
-        [[ -n "$token" ]] && break
+        fancy_print -n --print "*GH Auth Token: "
+        read -r GH_TOKEN
+        GH_TOKEN="${GH_TOKEN//[^a-zA-Z0-9]/}"
+        [[ -n "$GH_TOKEN" ]] && break
       done
-      export GH_TOKEN="$token"
     fi
     
     local try=0
     gh config set -h github.com git_protocol https
     while ! gh auth status &>/dev/null; do
       echo "$GH_TOKEN" | gh auth login --with-token &>/dev/null
-      ((try++)) && ((try>3)) && { status="failed"; break; }
+      [[ $? -ne 0 ]] && status="failed"; ((try++)); ((try>3)) && break
     done
     
-    [[ $? -eq 1 ]] && status="failed"
-    
+    echo
     FANCY_ARGS=(--preset="$status" +b)
-    echo; fancy_print "Login $status"
+    fancy_print "Login $status"
     [[ "$status" == "failed" ]] && exit 1
   fi
 }
@@ -205,9 +213,9 @@ publish_fancy() {
   git tag "$tag"
   git push --quiet origin "$tag"
   gh release create "$tag" \
-    --title "$DIST_REPO $tag" \
+    --title "$USER_REPO $tag" \
     --target "$(git rev-parse HEAD)" \
-    --repo "$DIST_OWNER/$DIST_REPO" >/dev/null
+    --repo "$USER_NAME/$USER_REPO" >/dev/null
     
   find "$OUT_DIR" -type f -iname '*.deb' \
     | while IFS= read -r file; do
@@ -220,25 +228,41 @@ publish_fancy() {
   done
 }
 
-install_pkgs curl gh git jq python3 tar unzip zip
-print_update_msg "checking updates for" 2
-gh_login && git pull --quiet &>/dev/null || exit 1
+update_script() {
+  install_pkgs curl gh git jq python3 tar unzip zip
+  print_update_msg "checking updates for" 2
+  cd "$MAIN_DIR" && gh_login || exit 1
+  
+  local before=$(git rev-parse HEAD)
+  git pull --quiet &>/dev/null
+  local after=$(git rev-parse HEAD)
+  
+  [[ "$before" == "$after" ]] && return
+  env ."$MAIN_DIR/update.sh" "${FORCE_UPDATE:-}"
+  exit 0
+}
 
-SRC_TOML=$(curl -fsSL "https://raw.githubusercontent.com/$SRC_OWNER/$SRC_REPO/refs/heads/master/Cargo.toml")
+FORCE_UPDATE="${1:-}"
+update_script
+
+: "${TMPDIR:=/tmp}"
+mkdir -p "$TMPDIR"
+
+SRC_TOML=$(curl -fsSL "https://raw.githubusercontent.com/$SRC_NAME/$SRC_REPO/refs/heads/master/Cargo.toml")
 LATEST_VERSION=$(get_ver "$SRC_TOML") CURRENT_VERSION=0
-[[ "$1" == '-f' ]] || CURRENT_VERSION=$(get_ver "$BUILD_SH")
+[[ "${FORCE_UPDATE:-}" == "-f" ]] || CURRENT_VERSION=$(get_ver "$BUILD_SH")
 if ! printf '%s\n' "$CURRENT_VERSION" "$LATEST_VERSION" | sort -V | tail -n1 | grep -xq "$CURRENT_VERSION"; then
   print_update_msg "updating" 1; echo
-  SRC_ZIP_URL="https://github.com/$SRC_OWNER/$SRC_REPO/archive/refs/heads/master.zip"
+  SRC_ZIP_URL="https://github.com/$SRC_NAME/$SRC_REPO/archive/refs/heads/master.zip"
   SRC_ZIP_SHA=$(curl -fsSL "${SRC_ZIP_URL}" | sha256sum | awk '{print $1}')
   { echo "$SRC_TOML" \
       | awk 'NR==1{next}/^ *$/{exit}NF{gsub(/\[|\]|"/,"")gsub(/ += +/,"=");print}' \
       | sed -E \
         -e "s/^(version|description)/_\1/" \
-        -e "s|^(homepage)=.*|_\1=https://github.com/$DIST_OWNER/$DIST_REPO|" \
+        -e "s|^(homepage)=.*|_\1=https://github.com/$USER_NAME/$USER_REPO|" \
         -e "/^license/{s|[0-9.-]||g; s|.*|_\U&|; p; s|_(.*)=(.*)/(.*)|_\1_file=\1-\2, \1-\3|}"
     printf '_%s=%s\n' \
-      srcurl "$SRC_ZIP_URL" sha256 "$SRC_ZIP_SHA" maintainer "$DIST_OWNER <$DIST_EMAIL>" \
+      srcurl "$SRC_ZIP_URL" sha256 "$SRC_ZIP_SHA" maintainer "$USER_NAME <$USER_EMAIL>" \
       breaks "$SRC_REPO" replaces "$SRC_REPO" build_in_src true auto_update true
   } | sed -E "/^_/!d; s|(.*)=|\Utermux_pkg\1=|;s|=(.*)|=\"\1\"|g" \
     | awk '{print length, $0}' | sort -nr | cut -d' ' -f2- > "$BUILD_SH"
